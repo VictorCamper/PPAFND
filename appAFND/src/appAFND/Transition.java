da86d7139cbe00bc853ac1f2eb1d31ac2cf5832a
felipeOL <felipe.olave.r@hotmail.com>
2017-04-07 15:23:53 -0300

    Agregación de clases
    En sus respectivos packages se agregaron las clases:
    Aphabet(no tiene nada)
    ,State( con sus atributos)
    ,Transition ( con sus atributos).
    ---
    controller: StateController
    ---
    model: aun nada ( creandose)
    ---
    view: StateView

---
+++ appAFND/src/appAFND/Transition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appAFND;
+
+/**
+ *
+ * @author felipe
+ */
+public class Transition
+{
+    public State from;
+    public State to;
+    public String label;
+    
+    
+}
