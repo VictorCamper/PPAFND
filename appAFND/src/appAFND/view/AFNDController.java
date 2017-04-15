be9be4ceab9f548ff07d41690fe3339bfb77c00f
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2017-04-14 21:32:23 -0300

    Drawing nodes with label
    We are drawing nodes on canvas.
    Scroll Pane > Canvas

--- appAFND/src/appAFND/view/AFNDController.java
+++ appAFND/src/appAFND/view/AFNDController.java
@@ -5,16 +5,18 @@
  */
 package appAFND.view;
 
-import appAFND.controller.*;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 
 /**
  * FXML Controller class
@@ -32,22 +34,23 @@ public class AFNDController implements Initializable {
     @FXML
     private Button button_transition;
     @FXML
-    private Button button_undo;
-    @FXML
-    private Button button_redo;
-    @FXML
     private ComboBox<?> combo_zoomLevel;
     @FXML
     private ScrollPane scrollPane;
     @FXML
     private Canvas canvas;
-
+    
+    
+    private int radius;
+    private ArrayList<Node> nodes;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        this.radius = 25;
+        this.nodes = new ArrayList<>();
     }    
 
     @FXML
@@ -55,7 +58,29 @@ public class AFNDController implements Initializable {
     }
 
     @FXML
-    private void drawState(ActionEvent event) {
+    private void drawState(MouseEvent event) {
+        GraphicsContext g = this.canvas.getGraphicsContext2D();
+        Node node = new Node(event.getX() - this.radius, event.getY() - this.radius);
+        
+        boolean overlapped = false;
+        
+        for(Node item : this.nodes)
+            if (item.compareTo(node) == 0)
+                overlapped = true;
+            
+        if(!overlapped)
+        {
+            g.strokeOval(event.getX() - this.radius, event.getY() - this.radius, this.radius*2, this.radius*2);
+            g.fillText("Q" + this.nodes.size(), event.getX(), event.getY());
+            /*
+            if(this.nodes.size() == 0)
+            {
+            // ADD INITIAL NODE
+            }
+            ADD NODES TO NFA
+            */
+            this.nodes.add(node);
+        }
     }
 
     @FXML
@@ -67,15 +92,44 @@ public class AFNDController implements Initializable {
     }
 
     @FXML
-    private void undo(ActionEvent event) {
-    }
-
-    @FXML
-    private void redo(ActionEvent event) {
-    }
-
-    @FXML
     private void zoom(ActionEvent event) {
+    }
+    
+    class Node implements Comparable<Node>
+    {
+        private double x;
+        private double y;
+
+        public Node(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+        
+        @Override
+        public int compareTo(Node o) {
+            
+            double distance = Math.sqrt(Math.pow(this.x - o.getX(),2) + Math.pow(this.y - o.getY(), 2));
+                        
+            if(distance <= (radius*2)) // Radius from outer class
+                return 0;
+            return 1;
+        }
     }
     
 }
